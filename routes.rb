d431297b23f7315e2d312fb1b2c22533c35e9077
Diana Kimball <diana.kimball@gmail.com>
2011-04-10 09:41:39 -0700

    Forgot a "do"...oopsies!

--- routes.rb
+++ routes.rb
@@ -5,6 +5,6 @@ get '/' do
   redirect 'http://blog.dianakimball.com'
 end
 
-get '/2011/03/roflcon-origins.html'
+get '/2011/03/roflcon-origins.html' do
   redirect 'http://www.dianakimball.com/2011/03/roflcon-origins.html'
 end

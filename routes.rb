f3d84f6c44b141186a06c59a5aa29896b0ea961f
Diana Kimball <diana.kimball@gmail.com>
2011-04-10 12:36:25 -0700

    Revert "Google doesn't seem to like having robots.txt be a redirect"
    This reverts commit 62694db334afcd94ba94cadbb108403e08e5f660.

--- routes.rb
+++ routes.rb
@@ -5,6 +5,10 @@ get '/' do
   redirect 'http://blog.dianakimball.com', 301
 end
 
+get '/robots.txt' do
+  redirect 'http://blog.dianakimball.com/robots.txt', 302
+end
+
 get '/sitemap.xml' do
   redirect 'http://blog.dianakimball.com/sitemap1.xml', 302
 end

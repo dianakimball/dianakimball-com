60574a552c693ac15e63fc8072f9f103093a32d3
Diana Kimball <diana.kimball@gmail.com>
2011-04-10 11:43:33 -0700

    Change redirects to use HTTP 301 status (302 is the default)

--- mapper.rb
+++ mapper.rb
@@ -8,7 +8,7 @@ File.open('routes.rb', "wb") do |file|
   CSV.foreach('map.csv') do |row|
     file.puts
     file.puts "get '#{row[0].sub(/^http:\/\/www.dianakimball.com/, '')}' do"
-    file.puts "  redirect '#{row[1]}'"
+    file.puts "  redirect '#{row[1]}', 301"
     file.puts "end"
   end
 end

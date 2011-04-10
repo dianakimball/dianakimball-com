fe31cc457b32aff90aeb360a23849b28373f9ab5
Diana Kimball <diana.kimball@gmail.com>
2011-04-10 10:00:35 -0700

    Generated Sinatra app from mapper

--- routes.rb
+++ routes.rb
@@ -6,5 +6,517 @@ get '/' do
 end
 
 get '/2011/03/roflcon-origins.html' do
-  redirect 'http://www.dianakimball.com/2011/03/roflcon-origins.html'
+  redirect 'http://www.dianakimball.blogspot.com/2011/03/roflcon-origins.html'
+end
+
+get '/2010/12/one-after-another.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2010/12/one-after-another.html'
+end
+
+get '/2010/12/global-ambitions-atlantas-1895-cotton.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2010/12/global-ambitions-atlantas-1895-cotton.html'
+end
+
+get '/2010/08/white-noise.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2010/08/white-noise.html'
+end
+
+get '/2010/05/in-between.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2010/05/in-between.html'
+end
+
+get '/2009/12/open-system.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/12/open-system.html'
+end
+
+get '/2009/12/capacity-for-love.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/12/capacity-for-love.html'
+end
+
+get '/2009/10/last-words.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/10/last-words.html'
+end
+
+get '/2009/09/every-word.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/09/every-word.html'
+end
+
+get '/2009/09/kickstarter-and-imminence.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/09/kickstarter-and-imminence.html'
+end
+
+get '/2009/08/all-at-once.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/08/all-at-once.html'
+end
+
+get '/2009/07/meanwhile-everything-else.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/07/meanwhile-everything-else.html'
+end
+
+get '/2009/05/paper-houses-vanity-doubt-and-perils-of.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/05/paper-houses-vanity-doubt-and-perils-of.html'
+end
+
+get '/2009/05/tim-diana-show-or-endings.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/05/tim-diana-show-or-endings.html'
+end
+
+get '/2009/04/world-of-writing.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/04/world-of-writing.html'
+end
+
+get '/2009/03/intimacy-of-imposition.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/03/intimacy-of-imposition.html'
+end
+
+get '/2009/02/what-you-wonder.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/02/what-you-wonder.html'
+end
+
+get '/2009/01/exposition.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2009/01/exposition.html'
+end
+
+get '/2008/12/nightmares-exorcised-in-dreamland.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/12/nightmares-exorcised-in-dreamland.html'
+end
+
+get '/2008/12/virginia.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/12/virginia.html'
+end
+
+get '/2008/12/calm-collision.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/12/calm-collision.html'
+end
+
+get '/2008/11/letters-from-artists.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/11/letters-from-artists.html'
+end
+
+get '/2008/09/harvard-business-school-22-accepted.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/09/harvard-business-school-22-accepted.html'
+end
+
+get '/2008/09/trying.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/09/trying.html'
+end
+
+get '/2008/09/dream-homes.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/09/dream-homes.html'
+end
+
+get '/2008/08/this-happened.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/08/this-happened.html'
+end
+
+get '/2008/08/blog-post.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/08/blog-post.html'
+end
+
+get '/2008/08/tall-letters-naked-words.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/08/tall-letters-naked-words.html'
+end
+
+get '/2008/07/in-absence-of-fiction.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/07/in-absence-of-fiction.html'
+end
+
+get '/2008/07/algorithms-and-avatars-or-what-ive.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/07/algorithms-and-avatars-or-what-ive.html'
+end
+
+get '/2008/07/sms-and-magic-numbers.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/07/sms-and-magic-numbers.html'
+end
+
+get '/2008/06/amusement-park-windmill-week-1-of-two.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/06/amusement-park-windmill-week-1-of-two.html'
+end
+
+get '/2008/06/locker-12-hbs-22-and-gmat.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/06/locker-12-hbs-22-and-gmat.html'
+end
+
+get '/2008/06/she-is-very-famous-for-her-donkey-tree.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/06/she-is-very-famous-for-her-donkey-tree.html'
+end
+
+get '/2008/06/lemonade-kool-aid-introducing-two-bits.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/06/lemonade-kool-aid-introducing-two-bits.html'
+end
+
+get '/2008/06/internet-vs-world-of-tomorrow.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/06/internet-vs-world-of-tomorrow.html'
+end
+
+get '/2008/05/viewmasters-deluxe.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/05/viewmasters-deluxe.html'
+end
+
+get '/2008/05/rip-old-headshot.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/05/rip-old-headshot.html'
+end
+
+get '/2008/05/magic-more-useful-than-home-economics.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/05/magic-more-useful-than-home-economics.html'
+end
+
+get '/2008/05/instructions-for-everyday-life.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/05/instructions-for-everyday-life.html'
+end
+
+get '/2008/05/roflcon-at-last.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/05/roflcon-at-last.html'
+end
+
+get '/2008/05/this-week-on-tim-diana-show.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/05/this-week-on-tim-diana-show.html'
+end
+
+get '/2008/04/dangerous-habit-now-in-kodachrome.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/04/dangerous-habit-now-in-kodachrome.html'
+end
+
+get '/2008/05/two-weeks-of-twitter.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/05/two-weeks-of-twitter.html'
+end
+
+get '/2008/04/roflcon-unknown-quantities.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/04/roflcon-unknown-quantities.html'
+end
+
+get '/2008/04/roflcon-in-beginning.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/04/roflcon-in-beginning.html'
+end
+
+get '/2008/04/roflcon-its-on.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/04/roflcon-its-on.html'
+end
+
+get '/2008/04/in-real-life.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/04/in-real-life.html'
+end
+
+get '/2008/03/watch-out-for-fallout.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/03/watch-out-for-fallout.html'
+end
+
+get '/2008/03/temptation-of-tumblr.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/03/temptation-of-tumblr.html'
+end
+
+get '/2008/03/cafs-or-silent-face-off.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/03/cafs-or-silent-face-off.html'
+end
+
+get '/2008/02/window-shopping-20.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/02/window-shopping-20.html'
+end
+
+get '/2008/02/photo-bicycle-in-pink.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/02/photo-bicycle-in-pink.html'
+end
+
+get '/2008/02/video-ghost-of-mario-or-how-i-learned.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/02/video-ghost-of-mario-or-how-i-learned.html'
+end
+
+get '/2008/02/its-easy-to-love-apple.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/02/its-easy-to-love-apple.html'
+end
+
+get '/2008/02/photo-baseball-shirt-brother-closet.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/02/photo-baseball-shirt-brother-closet.html'
+end
+
+get '/2008/02/making-mischief.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/02/making-mischief.html'
+end
+
+get '/2008/02/brother-grimm.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/02/brother-grimm.html'
+end
+
+get '/2008/02/practical-magic-and-american-cult-of.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/02/practical-magic-and-american-cult-of.html'
+end
+
+get '/2008/02/photo-like-view-from-airplane-in-spring.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/02/photo-like-view-from-airplane-in-spring.html'
+end
+
+get '/2008/02/seman-tics.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/02/seman-tics.html'
+end
+
+get '/2008/02/motorcycles-and-faces.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/02/motorcycles-and-faces.html'
+end
+
+get '/2008/01/lastfm-imeem.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/01/lastfm-imeem.html'
+end
+
+get '/2008/01/extra-shiny-friday-night-lights-or.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/01/extra-shiny-friday-night-lights-or.html'
+end
+
+get '/2008/01/boots-made-for-sloshing.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/01/boots-made-for-sloshing.html'
+end
+
+get '/2008/01/photo-view-from-her-room_05.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/01/photo-view-from-her-room_05.html'
+end
+
+get '/2008/01/phototastic.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2008/01/phototastic.html'
+end
+
+get '/2007/12/gipod-touch.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/12/gipod-touch.html'
+end
+
+get '/2007/12/fire-hydrants-siren-song.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/12/fire-hydrants-siren-song.html'
+end
+
+get '/2007/12/sound-unheard-2007-in-music.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/12/sound-unheard-2007-in-music.html'
+end
+
+get '/2007/12/runet-and-plight-of-transnational.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/12/runet-and-plight-of-transnational.html'
+end
+
+get '/2007/12/electro-smoking-robot-and-other-stories.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/12/electro-smoking-robot-and-other-stories.html'
+end
+
+get '/2007/12/igloos-and-insects.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/12/igloos-and-insects.html'
+end
+
+get '/2007/11/undercover.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/undercover.html'
+end
+
+get '/2007/11/digital-reluctants.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/digital-reluctants.html'
+end
+
+get '/2007/11/your-livestock-needs-protection-too.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/your-livestock-needs-protection-too.html'
+end
+
+get '/2007/11/i-have-seen-future.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/i-have-seen-future.html'
+end
+
+get '/2007/11/skitter-away.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/skitter-away.html'
+end
+
+get '/2007/11/symbolic-order.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/symbolic-order.html'
+end
+
+get '/2007/11/economics-of-procrastination.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/economics-of-procrastination.html'
+end
+
+get '/2007/11/4.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/4.html'
+end
+
+get '/2007/11/21st-century-shopping.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/21st-century-shopping.html'
+end
+
+get '/2007/11/keeping-secrets.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/keeping-secrets.html'
+end
+
+get '/2007/11/scratch-paper-and-scotch-tape.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/scratch-paper-and-scotch-tape.html'
+end
+
+get '/2007/11/mesmirizomatic.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/mesmirizomatic.html'
+end
+
+get '/2007/11/kindling.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/kindling.html'
+end
+
+get '/2007/11/futures-of-entertainment.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/futures-of-entertainment.html'
+end
+
+get '/2007/11/rofl.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/rofl.html'
+end
+
+get '/2007/11/harvard-business-school-22-getting-in.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/harvard-business-school-22-getting-in.html'
+end
+
+get '/2007/11/magicians-from-long-ago.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/magicians-from-long-ago.html'
+end
+
+get '/2007/11/text-me.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/text-me.html'
+end
+
+get '/2007/11/httpwwwbloggercomimggllinkgif.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/httpwwwbloggercomimggllinkgif.html'
+end
+
+get '/2007/11/like-open-book.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/like-open-book.html'
+end
+
+get '/2007/11/walt-mossberg-on-history.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/walt-mossberg-on-history.html'
+end
+
+get '/2007/11/postsecret-meets-getting-things-done.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/postsecret-meets-getting-things-done.html'
+end
+
+get '/2007/11/5-things-i-love-about-my-ipod-shuffle.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/5-things-i-love-about-my-ipod-shuffle.html'
+end
+
+get '/2007/11/ultimately-in-rainbows.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/ultimately-in-rainbows.html'
+end
+
+get '/2007/11/facebook-owns-my-soul.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/facebook-owns-my-soul.html'
+end
+
+get '/2007/11/hypnosis-through-stationery.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/hypnosis-through-stationery.html'
+end
+
+get '/2007/11/ipod-touch-jailbreak-now-playing-at.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/ipod-touch-jailbreak-now-playing-at.html'
+end
+
+get '/2007/11/leopardminuteurproductivity-magic.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/leopardminuteurproductivity-magic.html'
+end
+
+get '/2007/11/harvard-business-school-22-analyzed.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/harvard-business-school-22-analyzed.html'
+end
+
+get '/2007/11/futures-past.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/11/futures-past.html'
+end
+
+get '/2007/10/race-against-time.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/10/race-against-time.html'
+end
+
+get '/2007/10/pumpkins.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/10/pumpkins.html'
+end
+
+get '/2007/10/whats-russian-for-payback.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/10/whats-russian-for-payback.html'
+end
+
+get '/2007/10/paying-attention-new-old-business-model.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/10/paying-attention-new-old-business-model.html'
+end
+
+get '/2007/10/rsum-revolution.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/10/rsum-revolution.html'
+end
+
+get '/2007/09/this-way-to-providence.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/09/this-way-to-providence.html'
+end
+
+get '/2007/09/metahipster-manifest.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/09/metahipster-manifest.html'
+end
+
+get '/2007/09/xo-my-favorite-little-monster.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/09/xo-my-favorite-little-monster.html'
+end
+
+get '/2007/09/long-layover-in-flat-world.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/09/long-layover-in-flat-world.html'
+end
+
+get '/2007/09/microsoft-v-world-its-on.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/09/microsoft-v-world-its-on.html'
+end
+
+get '/2007/09/short-personal-narrative-followed-by.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/09/short-personal-narrative-followed-by.html'
+end
+
+get '/2007/09/laptops-covered-in-bumper-stickers.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/09/laptops-covered-in-bumper-stickers.html'
+end
+
+get '/2007/09/liferemix-remixed.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/09/liferemix-remixed.html'
+end
+
+get '/2007/09/photograph-lottery.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/09/photograph-lottery.html'
+end
+
+get '/2007/09/schoolhouse-rocks.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/09/schoolhouse-rocks.html'
+end
+
+get '/2007/09/photographs-from-long-ago.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/09/photographs-from-long-ago.html'
+end
+
+get '/2007/08/livejournal-1907.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/08/livejournal-1907.html'
+end
+
+get '/2007/08/gtd-remixed.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/08/gtd-remixed.html'
+end
+
+get '/2007/08/your-very-own-common-seal-or-nypl.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/08/your-very-own-common-seal-or-nypl.html'
+end
+
+get '/2007/08/questions-and-answers.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/08/questions-and-answers.html'
+end
+
+get '/2007/08/college-high-school-or-6-ways-to-get.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/08/college-high-school-or-6-ways-to-get.html'
+end
+
+get '/2007/08/that-enchanted-bookmobile-or-826-boston.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/08/that-enchanted-bookmobile-or-826-boston.html'
+end
+
+get '/2007/08/google-me-generation.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/08/google-me-generation.html'
+end
+
+get '/2007/08/notely-and-lure-of-one-stop-shop.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/08/notely-and-lure-of-one-stop-shop.html'
+end
+
+get '/2007/08/so-magical-right-now.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/08/so-magical-right-now.html'
+end
+
+get '/2007/08/basic-principles.html' do
+  redirect 'http://www.dianakimball.blogspot.com/2007/08/basic-principles.html'
 end
